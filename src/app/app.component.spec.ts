1f8a1e6a753cdf37eef9628fccb2223aa22ebacb
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-03-22 22:23:00 -0400

    feat: add header component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-store'`, () => {
+  it(`should have as title 'Best Store 2.0'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-store');
+    expect(app.title).toEqual('Best Store 2.0');
   });
 
   it('should render title', () => {
