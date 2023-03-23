293fa4c86784ff35d2332a7c96c6d4c5db207065
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-03-22 23:38:27 -0400

    feat: add footer and product components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductComponent } from './components/product/product.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    ProductComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
